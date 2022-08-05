cfb2d9571efee1295bb52816658636a3a36e4020
jcholly4 <96598953+jcholly4@users.noreply.github.com>
2022-08-04 23:51:42 -0400

    Add files via upload

---
+++ farenheit to celsius conversion.py
@@ -0,0 +1,14 @@
+temperature = float(input("Enter Farenheit:"))
+print("The Farenheit is: " + str(temperature))
+c = (temperature - 32) * 5/9
+print("The Celsius is: ", end=" ")
+print(c)
+
+
+
+temp = input("Enter Celsius:")
+print("The Celsius is: " + temp)
+temp = float(temp)
+f = (temp * 9/5) + 32
+print("The Farenheit is: ", end=" ")
+print(f)
